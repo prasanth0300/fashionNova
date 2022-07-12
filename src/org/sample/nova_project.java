5e40771db575b251658b0f12d57f20aa89a1b0ff
prasanth0300 <prasanthcena3@gmail.com>
2022-07-12 12:23:21 +0530

    fresh code added

--- src/org/sample/nova_project.java
+++ src/org/sample/nova_project.java
@@ -16,4 +16,16 @@ public class nova_project {
 	private void sports_wear() {
 		System.out.println("sports wear collection");
 	}
+
+	private void animals_wear() {
+		System.out.println("animals wear collection");
+	}
+	public static void main(String[] args) {
+		nova_project np=new nova_project();
+		
+		np.animals_wear();
+		np.kids_wear();
+		np.mens_wear();
+		np.girls_wear();
+	}
 }
