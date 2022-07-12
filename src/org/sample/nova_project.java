61eed85bfcaf6719cd963d126585586c8f931685
prasanth0300 <prasanthcena3@gmail.com>
2022-07-12 12:08:39 +0530

    nova code added

---
+++ src/org/sample/nova_project.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class nova_project {
+	private void girls_wear() {
+		System.out.println("ladies new collections");
+	}
+
+	private void mens_wear() {
+		System.out.println("mens new collections");
+	}
+}
